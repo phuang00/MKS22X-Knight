84796f66a3a3279e4cd67ee8e5c613aa73d2444b
phuang00 <phuang00@stuy.edu>
2019-02-12 15:43:08 -0500

    copied method headers from website

---
+++ KnightBoard.java
@@ -0,0 +1,27 @@
+public class KnightBoard{
+  /*
+  Initialize the board to the correct size and make them all 0's
+  @throws IllegalArgumentException when either parameter is negative.
+  */
+  public KnightBoard(int startingRows,int startingCols){}
+
+  public String toString(){}
+
+  /*
+  @throws IllegalStateException when the board contains non-zero values.
+  @throws IllegalArgumentException when either parameter is negative
+   or out of bounds.
+   */
+  public boolean solve(int startingRow, int startingCol){}
+
+  /*
+  @throws IllegalStateException when the board contains non-zero values.
+  @throws IllegalArgumentException when either parameter is negative
+   or out of bounds.
+   */
+  public int countSolutions(int startingRow, int startingCol){}
+
+
+  private boolean solveH(int row ,int col, int level){}
+  // level is the # of the knight
+}
