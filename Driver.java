81da9b0b19021c643b23c5e6adc5af25c4e0b539
phuang00 <phuang00@stuy.edu>
2019-02-12 20:48:28 -0500

    tested constructor and the working part of toString

---
+++ Driver.java
@@ -0,0 +1,8 @@
+public class Driver{
+  public static void main(String[] args) {
+    KnightBoard a = new KnightBoard(10,10);
+    System.out.println(a);
+    a = new KnightBoard(3,3);
+    System.out.println(a);
+  }
+}
