7954cfcfde79c4b78126e565209dae4899dbed3a
phuang00 <phuang00@stuy.edu>
2019-02-13 11:10:05 -0500

    added commented to toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -13,15 +13,22 @@ public class KnightBoard{
   public String toString(){
     String digits = "_";
     if (board.length * board[0].length > 10) digits = "__";
+    // if the row * columns >= 10, default digits is "__" instead of "_"
     String ans = "";
     for (int i = 0; i < board.length; i++){
       for (int j = 0; j < board[i].length; j++){
         int temp = board[i][j];
         if (temp == 0) ans += digits;
+        // if there is no knight on that spot, add the default underscores to the ans
         else if (temp < 10 && digits.length() == 2) ans += "_" + temp;
+        // else if the knight on the spot is less than ten but the row * columns has more than one digit,
+        // add an underscore and the number of the knight
         else ans += temp;
+        // else just add the number of the knight to the ans
         if (j != board[i].length - 1) ans += " ";
+        // if it's not the last column on the row, add a space
         else ans += '\n';
+        // else add '\n'
       }
     }
     return ans;
